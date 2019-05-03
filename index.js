2305d8e3733897d2949a85f92a54697d64e22bb3
Michael Johnson <migarjo@github.com>
2019-05-03 16:19:31 -0600

    Only act on issue_transfer events

--- index.js
+++ index.js
@@ -30,33 +30,33 @@ app.post('/github-app', function(req, res) {
 })
 
 app.post('/', function(req, res) {
-    console.dir(req.body)
-
-    githubApp.asApp().then(github => {
-        github.apps.getInstallations({}).then(installations => {
-            console.log("Installations:")
-            console.log(installations.data)
-            var installation = installations.data.find(install => {
-                return install.account.login === req.body.organization
-            })
-            var commentText = "This issue was moved to " + req.body.to_pipeline_name + " on ZenHub."
-            console.log("Installation:", installation.id)
-            githubApp.asInstallation(installation.id).then(client => {
-                client.issues.createComment({
-                    owner: req.body.organization,
-                    repo: req.body.repo,
-                    number: req.body.issue_number,
-                    body: commentText
-                }).then(({ data }) => {
-                    console.log(data)
-                }).catch((err) => {
-                    console.log("ERROR:", err)
+    
+    if (req.body.type == "issue_transfer") {
+        githubApp.asApp().then(github => {
+            github.apps.getInstallations({}).then(installations => {
+                console.log("Installations:")
+                console.log(installations.data)
+                var installation = installations.data.find(install => {
+                    return install.account.login === req.body.organization
+                })
+                var commentText = "This issue was moved to " + req.body.to_pipeline_name + " on ZenHub."
+                console.log("Installation:", installation.id)
+                githubApp.asInstallation(installation.id).then(client => {
+                    client.issues.createComment({
+                        owner: req.body.organization,
+                        repo: req.body.repo,
+                        number: req.body.issue_number,
+                        body: commentText
+                    }).then(({ data }) => {
+                        console.log(data)
+                    }).catch((err) => {
+                        console.log("ERROR:", err)
+                    })
                 })
             })
-
         })
-
-    })
+    }
+    
 
 
 
