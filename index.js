92ee288f021d182da5e159255a002e9029b9fc16
Glitch (vila89-zenhub-webhook-demo) <none>
2019-05-07 22:18:21 +0000

    🌄🚘 Updated with Glitch

--- index.js
+++ index.js
@@ -29,6 +29,10 @@ app.post('/github-app', function(req, res) {
     res.sendStatus(200)
 })
 
+app.get('/', function(req, res) {
+res.send('Hello! We are up and running!')
+})
+        
 app.post('/', function(req, res) {
     
     if (req.body.type == "issue_transfer") {
@@ -40,6 +44,30 @@ app.post('/', function(req, res) {
                     return install.account.login === req.body.organization
                 })
                 var commentText = "This issue was moved to " + req.body.to_pipeline_name + " on ZenHub."
+                console.log("Installation:", installation.id)
+                githubApp.asInstallation(installation.id).then(client => {
+                    client.issues.createComment({
+                        owner: req.body.organization,
+                        repo: req.body.repo,
+                        number: req.body.issue_number,
+                        body: commentText
+                    }).catch((err) => {
+                        console.log("ERROR:", err)
+                    })
+                })
+            })
+        })
+    }
+      
+  if (req.body.type == "estimate_set") {
+        githubApp.asApp().then(github => {
+            github.apps.getInstallations({}).then(installations => {
+                console.log("Installations:")
+                console.log(installations.data)
+                var installation = installations.data.find(install => {
+                    return install.account.login === req.body.organization
+                })
+                var commentText = "The estimate was changed to " + req.body.estimate + " on ZenHub."
                 console.log("Installation:", installation.id)
                 githubApp.asInstallation(installation.id).then(client => {
                     client.issues.createComment({
